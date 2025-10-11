317ca24fe7b06643c5790252dc2d4cf9e3e511b2
Kayden Tarvaran <ktarvaran0@saddleback.edu>
2025-10-11 01:01:44 -0700

    added files

---
+++ bookType.cpp
@@ -0,0 +1,8 @@
+/*********************************************************************
+* CS1B – G3: Serendipity
+*  Partner A: Sean Hanabusa (ID) — role: Cashier.cpp
+*  Partner B: Kayden Tarvaran (ID) — role: invmenu.cpp, main.cpp, reports.cpp
+*  Date: 2025‑09‑18
+*  Purpose: Design and implement menu-driven input with validation for an inventory system.
+*  Build:   g++ -std=c++20 mainmenu.cpp cashier.cpp invmenu.cpp reports.cpp -o serendipity
+***********************************************************************/
